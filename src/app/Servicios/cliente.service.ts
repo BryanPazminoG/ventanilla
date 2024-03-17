63a01e118a485086c1f8a73a6a87f3fe7b45779d
RUBEN ALFREDO TOMALA VERA <ruben.al97@hotmail.com>
2024-03-17 00:06:16 -0500

    Cambios CLiente

--- src/app/Servicios/cliente.service.ts
+++ src/app/Servicios/cliente.service.ts
@@ -7,14 +7,16 @@ import { Observable } from 'rxjs';
 })
 export class ClienteService {
   //private buscarClienteApi = "http://localhost:8081/api/v1/clientes";
-  
+
   private buscarClienteApi = "http://35.192.130.249:8081/api/v1/clientes";
+  private clienteByIdApi = 'http://34.123.168.16:8080/api/v1/clientes/naturales/';
 
   constructor(private http: HttpClient) { }
 
   buscarClientePorId(id: string): Observable<any> {
-    let url = `${this.buscarClienteApi}/${id}`;
-    return this.http.get<any>(url);
+    const params = new HttpParams()
+      .set('id', id);
+    return this.http.get<any>(this.clienteByIdApi + id);
   }
 
   buscarClientePorIdentificacion(tipo: string, numero: string): Observable<any> {
